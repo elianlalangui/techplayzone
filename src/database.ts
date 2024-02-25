8bcfdfc7f1742b2bdf477ce35fa2633c9910d820
Elian Lalangui <89472619+elianlalangui@users.noreply.github.com>
2024-02-25 11:00:55 -0500

    Update database.ts

--- src/database.ts
+++ src/database.ts
@@ -5,5 +5,7 @@ export const pool = new Pool({
     host:'pg-techplayzone-elianlalangui-648f.a.aivencloud.com',
     password: 'AVNS_bB_El9iT8VQ5W9SyMOB',
     database: 'defaultdb',
-    port: 22247
+    port: 22247,
+    sslmode: 'requiere',
+    ssl: {rejectUnauthorized: false}
 })
